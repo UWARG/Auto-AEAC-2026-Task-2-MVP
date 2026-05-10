c9b58f5551bf3542a19e515cee243e5219eec591
honzikschenk <nojschenk@gmail.com>
2026-05-10 19:32:15 -0400

    log 1-4 rc

--- airside.py
+++ airside.py
@@ -575,7 +575,8 @@ def main(
             spray_switch = spray_raw >= 1800
             delta = time.time() - last_event_time
 
-            logging.info(f"spray: raw={spray_raw}, switch={'on' if spray_switch else 'off'}, delta={delta:.2f}s")
+            # logging.info(f"spray: raw={spray_raw}, switch={'on' if spray_switch else 'off'}, delta={delta:.2f}s")
+            logging.info(f"{mav.get_rc_channel(1).raw=} {mav.get_rc_channel(2).raw=} {mav.get_rc_channel(3).raw=} {mav.get_rc_channel(4).raw=}")
 
             # Handle spray deactivation
             if spray_active and (not spray_switch or delta >= SPRAY_DURATION_SEC):
